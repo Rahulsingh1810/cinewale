150a5aca12aedb54f1d6df1521cdb816cba640b2
Rahulsingh1810 <rahul181002singh@gmail.com>
2023-12-18 21:57:27 +0530

    Initial commit

---
+++ src/components/Pagination/Pagination.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+
+const Pagination = ({ currentPage, totalPages, setPage }) => {
+  if (totalPages === 0) return null;
+
+  const handlePrev = () => {
+    if (currentPage !== 1) {
+      setPage((prevPage) => prevPage - 1);
+    }
+  };
+
+  const handleNext = () => {
+    if (currentPage !== totalPages) {
+      setPage((prevPage) => prevPage + 1);
+    }
+  };
+
+  return (
+    <div className="mb-5 flex items-center justify-center">
+      <button
+        className="me-3 flex h-8 items-center justify-center rounded-lg bg-[#227fb4] px-3 text-sm text-white hover:bg-[#175a81]"
+        onClick={handlePrev}
+      >
+        Previous
+      </button>
+      <button
+        className="me-3 flex h-8 items-center justify-center rounded-lg bg-[#227fb4] px-3 text-sm text-white hover:bg-[#175a81]"
+        onClick={handleNext}
+      >
+        Next
+      </button>
+    </div>
+  );
+};
+
+export default Pagination;
---
+++ src/services/Api.js
@@ -0,0 +1,40 @@
+// Here we make all the calls to TMDB API
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+// const page = 1;
+
+export const tmdbAPI = createApi({
+  reducerPath: "tmdbAPI",
+  baseQuery: fetchBaseQuery({ baseUrl: "https://api.themoviedb.org/3/" }),
+  endpoints: (builder) => ({
+
+    // Get Genres
+    getGenres: builder.query({
+      query: () => `genre/movie/list?api_key=c643a6685dfefa1bd8e9df8ab94f6004`,
+    }),
+    // Get movies by type
+    getMovies: builder.query({
+      query: ({ genreName, page, searchQuery }) => {
+
+        // Search movies
+        if(searchQuery){
+          return `/search/movie?query=${searchQuery}&page=${page}&api_key=c643a6685dfefa1bd8e9df8ab94f6004`
+      }
+
+        // Get movies by Genre
+        if (genreName && typeof genreName === 'number') {
+          return `discover/movie?with_genres=${genreName}&page=${page}&api_key=c643a6685dfefa1bd8e9df8ab94f6004`
+        }
+        // Get Popular Movies
+        return `movie/popular?page=${page}&api_key=c643a6685dfefa1bd8e9df8ab94f6004`;
+      }
+    }),
+    // Get Movie
+    getMovie: builder.query({
+      query: (id) =>
+        `/movie/${id}?append_to_response=videos,credits&api_key=c643a6685dfefa1bd8e9df8ab94f6004`,
+    }),
+  }),
+});
+
+export const { useGetMoviesQuery, useGetMovieQuery, useGetGenresQuery } = tmdbAPI;
